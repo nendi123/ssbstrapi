d8a8a642a9ddb8df82858b315391d3b7546d1c6d
nendi123 <nendi@niagateknologi.net>
2022-04-25 03:28:58 +0700

    Initial Commit

---
+++ src/api/training-schedule/controllers/training-schedule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  training-schedule controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::training-schedule.training-schedule');
---
+++ src/api/training-schedule/routes/training-schedule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * training-schedule router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::training-schedule.training-schedule');
